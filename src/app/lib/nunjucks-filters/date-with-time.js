cd4c1a14d608f861d8547ef47294a19715de65d4
Graham Veal <grahamveal@gmail.com>
2018-06-18 11:36:50 +0100

    Add time to report started column on homepage
    Add new nunjucks filter with tests to show date and time
    Use new filter on homepage

---
+++ src/app/lib/nunjucks-filters/date-with-time.js
@@ -0,0 +1,20 @@
+const dateFormat = require( 'dateformat' );
+
+function isValid( datestr ){
+
+	return ( typeof datestr === 'number' ) || !!Date.parse( datestr );
+}
+
+module.exports = function( datestr ){
+
+	let date;
+
+	if( datestr && isValid( datestr ) ){
+
+		date = new Date( datestr );
+		return dateFormat( date, 'GMT:d mmmm yyyy "at" h:MMtt' );
+
+	}
+
+	return datestr;
+};
--- src/app/lib/nunjucks-filters/index.js
+++ src/app/lib/nunjucks-filters/index.js
@@ -3,4 +3,5 @@ module.exports = function( env ){
 	env.addFilter( 'highlight', require( './highlight' ) );
 	env.addFilter( 'removeEmpty', require( './remove-empty' ) );
 	env.addFilter( 'dateOnly', require( './date-only' ) );
+	env.addFilter( 'dateWithTime', require( './date-with-time' ) );
 };
