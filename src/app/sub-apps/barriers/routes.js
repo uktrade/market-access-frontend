5b5162f43edc6dbd1b68c562e8a65e6d87ad85a7
Graham <grahamveal@gmail.com>
2018-07-23 16:45:30 +0100

    Release v3.2.0 (#29)
    * Add Interactions and events to barrier details
    
    Refactor barrier detail to abstract into a layout to re-use
    Add new urls
    Add new service methods
    Add new controllers to render interactions and add a note
    Create new event-list component
    
    * Reduce code coverage minimum for now
    
    * 3.2.0

--- src/app/sub-apps/barriers/routes.js
+++ src/app/sub-apps/barriers/routes.js
@@ -1,13 +1,20 @@
+const csurf = require( 'csurf' );
 const controller = require( './controllers' );
 
 const barrierId = require( './middleware/params/barrier-id' );
 
+const csrfProtection = csurf();
 
 module.exports = ( express, app ) => {
+
+	const parseBody = express.urlencoded( { extended: false } );
 
 	app.param( 'barrierId', barrierId );
 
 	app.get( '/:barrierId/', controller.barrier );
+	app.get( '/:barrierId/interactions/', controller.interactions );
+	app.get( '/:barrierId/interactions/add-note', csrfProtection, controller.addNote );
+	app.post( '/:barrierId/interactions/add-note', parseBody, csrfProtection, controller.addNote );
 
 	return app;
 };
