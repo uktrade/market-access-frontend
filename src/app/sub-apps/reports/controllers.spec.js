a3fc889fcb0818ba5e1d8230cb9e396e71e2dd9a
Graham Veal <grahamveal@gmail.com>
2018-10-17 09:05:58 +0100

    Add missing tests for report sectors

--- src/app/sub-apps/reports/controllers.spec.js
+++ src/app/sub-apps/reports/controllers.spec.js
@@ -94,7 +94,8 @@ describe( 'Report controller', () => {
 				getAll: jasmine.createSpy( 'backend.reports.getAll' ),
 				getForCountry: jasmine.createSpy( 'backend.reports.getForCountry' ),
 				getAllUnfinished: jasmine.createSpy( 'backend.reports.getAllUnfinished' ),
-				saveHasSectors: jasmine.createSpy( 'backend.reports.saveHasSectors' )
+				saveHasSectors: jasmine.createSpy( 'backend.reports.saveHasSectors' ),
+				saveSectors: jasmine.createSpy( 'backend.reports.saveSectors' )
 			}
 		};
 		datahub = {
@@ -920,6 +921,165 @@ describe( 'Report controller', () => {
 		} );
 	} );
 
+	describe( 'sectors', () => {
+
+		const template = 'reports/views/sectors';
+
+		beforeEach( () => {
+
+			req.report = {};
+		} );
+
+		function checkRender( sectors ){
+
+			expect( res.render ).toHaveBeenCalledWith( template, { sectors: sectors.map( metadata.getSector ), csrfToken } );
+		}
+
+		describe( 'a GET', () => {
+			describe( 'With sectors in the session', () => {
+				it( 'Should render the page with the sectors', async () => {
+
+					const sectors = [ uuid(), uuid(), uuid() ];
+
+					req.session.sectors = sectors;
+
+					await controller.sectors( req, res, next );
+
+					checkRender( sectors );
+				} );
+			} );
+
+			describe( 'With sectors on the report', () => {
+				it( 'Should render the page with the sectors', async () => {
+
+					const sectors = [ uuid(), uuid(), uuid() ];
+
+					req.report.sectors = sectors;
+
+					await controller.sectors( req, res, next );
+
+					checkRender( sectors );
+				} );
+			} );
+
+			describe( 'With no sectors', () => {
+				it( 'Should render the page with and empty list', async () => {
+
+					await controller.sectors( req, res, next );
+
+					checkRender( [] );
+				} );
+			} );
+		} );
+
+		describe( 'a POST', () => {
+
+			beforeEach( () => {
+
+				req.method = 'POST';
+				req.body = {};
+			} );
+
+			describe( 'With no sectors', () => {
+				it( 'Should render the page', async () => {
+
+					await controller.sectors( req, res, next );
+
+					checkRender( [] );
+				} );
+			} );
+
+			describe( 'With an empty list of sectors', () => {
+				it( 'Should render the page', async () => {
+
+					await controller.sectors( req, res, next );
+
+					checkRender( [] );
+				} );
+			} );
+
+			describe( 'With sectors', () => {
+
+				beforeEach( () => {
+
+					req.session.sectors = [ uuid(), uuid() ];
+				} );
+
+				describe( 'When the service throws an error', () => {
+					it( 'Should call next with the error', async () => {
+
+						const err = new Error( 'boom' );
+						backend.reports.saveSectors.and.callFake( () => { throw err; } );
+
+						await controller.sectors( req, res, next );
+
+						expect( next ).toHaveBeenCalledWith( err );
+						expect( res.render ).not.toHaveBeenCalled();
+						expect( req.session.sectors ).not.toBeDefined();
+					} );
+				} );
+
+				describe( 'When the service response is not a success', () => {
+					it( 'Should call next with an error', async () => {
+
+						const statusCode = 500;
+						const err = new Error( `Unable to update report, got ${ statusCode } response code` );
+
+						backend.reports.saveSectors.and.callFake( () => Promise.resolve( { response: { isSuccess: false, statusCode } } ) );
+
+						await controller.sectors( req, res, next );
+
+						expect( next ).toHaveBeenCalledWith( err );
+						expect( res.render ).not.toHaveBeenCalled();
+						expect( req.session.sectors ).not.toBeDefined();
+					} );
+				} );
+
+				describe( 'When the service response is success', () => {
+
+					beforeEach( () => {
+
+						backend.reports.saveSectors.and.callFake( () => ({ response: { isSuccess: true } }) );
+						req.report.id = uuid();
+					} );
+
+					describe( 'When it is an exit', () => {
+						it( 'Should redirect to the detail page', async () => {
+
+							const detailResponse = '/a/detail/';
+
+							urls.reports.detail.and.callFake( () => detailResponse );
+							req.body = { action: 'exit' };
+
+							await controller.sectors( req, res, next );
+
+							expect( next ).not.toHaveBeenCalled();
+							expect( res.render ).not.toHaveBeenCalled();
+							expect( res.redirect ).toHaveBeenCalledWith( detailResponse );
+							expect( urls.reports.detail ).toHaveBeenCalledWith( req.report.id );
+						} );
+					} );
+
+					describe( 'When it is NOT an exit', () => {
+						it( 'Should redirect to the aboutProblem page', async () => {
+
+							const aboutResponse = '/about/report/';
+
+							urls.reports.aboutProblem.and.callFake( () => aboutResponse );
+
+							await controller.sectors( req, res, next );
+
+							expect( next ).not.toHaveBeenCalled();
+							expect( res.render ).not.toHaveBeenCalled();
+							expect( res.redirect ).toHaveBeenCalledWith( aboutResponse );
+							expect( urls.reports.aboutProblem ).toHaveBeenCalledWith( req.report.id );
+						} );
+					} );
+				} );
+			} );
+		} );
+	} );
+
 	describe( 'addSector', () => {
 
 		let report;
@@ -1059,7 +1219,7 @@ describe( 'Report controller', () => {
 		} );
 	} );
 
-	describe( 'Remove sector', () => {
+	describe( 'removeSector', () => {
 		it( 'Should remove the sector in the session list and redirect', () => {
 
 			const sector = uuid();
