516b1c5f579fe3015f022bec06091f459fb13cdb
Graham Veal <grahamveal@gmail.com>
2019-07-01 09:41:04 +0100

    Add missing tests for country id middleware param

--- src/app/sub-apps/reports/middleware/params/country-id.js
+++ src/app/sub-apps/reports/middleware/params/country-id.js
@@ -1,9 +1,13 @@
 const { isCountry } = require( '../../../../lib/validators' );
 
 module.exports = async ( req, res, next, countryId ) => {
-    if (isCountry(countryId)){
-        next();
-    } else {
-        next( new Error( 'Invalid countryId' ) );
-    }
+
+	if( isCountry( countryId ) ){
+
+		next();
+
+	} else {
+
+		next( new Error( 'Invalid countryId' ) );
+	}
 };
